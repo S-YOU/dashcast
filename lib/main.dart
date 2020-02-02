c57cc78ad99d8359a873bf3067ec3015c0d15731
Piotr <piotrekert90@gmail.com>
2020-02-02 21:01:10 +0100

    Add BoringPage and PlaybackButton widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,46 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold(),
+      home: BoringPage(),
     );
   }
 }
 
+class BoringPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: PlaybackButtton()),
+    );
+  }
+}
+
+class PlaybackButtton extends StatefulWidget {
+  @override
+  _PlaybackButttonState createState() => _PlaybackButttonState();
+}
+
+class _PlaybackButttonState extends State<PlaybackButtton> {
+  bool _isPlaying = false;
+
+  void stop() {}
+
+  void play() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(_isPlaying ? Icons.stop : Icons.play_arrow),
+      onPressed: () => _playSound(),
+    );
+  }
+
+  void _playSound() {
+    if (_isPlaying) {
+      stop();
+    } else {
+      play();
+    }
+    setState(() => _isPlaying = !_isPlaying);
+  }
+}
