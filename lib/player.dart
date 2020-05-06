f3e826957a6f446ffe4ceac4ef39e771376b8820
Piotr <piotrekert90@gmail.com>
2020-05-06 18:51:02 +0200

    Move Player related stuff to seperate file.

---
+++ lib/player.dart
@@ -0,0 +1,147 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:provider/provider.dart';
+import 'package:dashcast/main.dart';
+
+class PlayerPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          Provider.of<Podcast>(context).selectedItem.title,
+        ),
+      ),
+      body: SafeArea(child: Player()),
+    );
+  }
+}
+
+class Player extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final podcast = Provider.of<Podcast>(context);
+    return Column(
+      children: [
+        Flexible(
+            flex: 8,
+            child: SingleChildScrollView(
+              child: Column(children: [
+                Image.network(podcast.feed.image.url),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    podcast.selectedItem.description.trim(),
+                  ),
+                ),
+              ]),
+            )),
+        Flexible(
+          flex: 2,
+          child: Material(
+            elevation: 12,
+            child: AudioControls(),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class AudioControls extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        PlaybackButtons(),
+      ],
+    );
+  }
+}
+
+class PlaybackButtons extends StatefulWidget {
+  @override
+  _PlaybackButtonState createState() => _PlaybackButtonState();
+}
+
+class _PlaybackButtonState extends State<PlaybackButtons> {
+  bool _isPlaying = false;
+  FlutterSound _sound;
+  double _playPosition;
+  StreamSubscription<PlayStatus> _playerSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _sound = FlutterSound();
+    _playPosition = 0;
+  }
+
+  @override
+  void dispose() {
+    _cleanup();
+    super.dispose();
+  }
+
+  void _cleanup() async {
+    if (_sound.audioState == t_AUDIO_STATE.IS_PLAYING)
+      await _sound.stopPlayer();
+    _playerSubscription.cancel();
+  }
+
+  void _stop() async {
+    await _sound.stopPlayer();
+    setState(() => _isPlaying = false);
+  }
+
+  void _play(String url) async {
+    await _sound.startPlayer(url);
+    _playerSubscription = _sound.onPlayerStateChanged.listen((e) {
+      if (e != null) {
+        print(e.currentPosition);
+        setState(() => _playPosition = (e.currentPosition / e.duration));
+      }
+    });
+    setState(() => _isPlaying = true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final item = Provider.of<Podcast>(context).selectedItem;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Slider(
+          value: _playPosition,
+          onChanged: null,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.fast_rewind),
+              onPressed: null,
+            ),
+            IconButton(
+              icon: _isPlaying ? Icon(Icons.stop) : Icon(Icons.play_arrow),
+              onPressed: () {
+                if (_isPlaying) {
+                  _stop();
+                } else {
+                  _play(item.guid);
+                }
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.fast_forward),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
